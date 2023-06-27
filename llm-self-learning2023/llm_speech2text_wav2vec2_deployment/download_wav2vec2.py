f9d05298cb46ab4043e3d7ce173fc6faa891532a
Yajing <52061989+Yajing-Z@users.noreply.github.com>
2023-06-27 17:19:02 +0800

    Update download_wav2vec2.py
    Fix https error

--- llm-self-learning2023/llm_speech2text_wav2vec2_deployment/download_wav2vec2.py
+++ llm-self-learning2023/llm_speech2text_wav2vec2_deployment/download_wav2vec2.py
@@ -3,6 +3,11 @@
 from transformers import AutoModelForCTC, AutoProcessor
 import os
 
+# Fix: requests.exceptions.SSLError: HTTPSConnectionPool(host='huggingface.co', port=443): Max retries exceeded with url....aused by SSLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:1129)'
+os.environ['HTTP_PROXY'] = 'http://proxy.vmware.com:3128'
+os.environ['HTTPS_PROXY'] = 'http://proxy.vmware.com:3128'
+
+
 modelname = "facebook/wav2vec2-base-960h"
 model = AutoModelForCTC.from_pretrained(modelname)
 processor = AutoProcessor.from_pretrained(modelname)
