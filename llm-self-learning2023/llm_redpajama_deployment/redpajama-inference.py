f3e99b0e4101fee364f7c0eef14ef307a301490a
Yajing <52061989+Yajing-Z@users.noreply.github.com>
2023-06-27 17:12:17 +0800

    Update redpajama-inference.py
    fix https error: add proxy environment variables

--- llm-self-learning2023/llm_redpajama_deployment/redpajama-inference.py
+++ llm-self-learning2023/llm_redpajama_deployment/redpajama-inference.py
@@ -5,14 +5,19 @@ import torch
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# Fix: requests.exceptions.SSLError: HTTPSConnectionPool(host='huggingface.co', port=443): Max retries exceeded with url....aused by SSLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:1129)'
+import os
+os.environ['HTTP_PROXY'] = 'http://proxy.vmware.com:3128'
+os.environ['HTTPS_PROXY'] = 'http://proxy.vmware.com:3128'
+
 MIN_TRANSFORMERS_VERSION = '4.25.1'
 
 # check transformers version
 assert transformers.__version__ >= MIN_TRANSFORMERS_VERSION, f'Please upgrade transformers to version {MIN_TRANSFORMERS_VERSION} or higher.'
 
 # init
-tokenizer = AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Instruct-3B-v1", local_files_only=True)
-model = AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Instruct-3B-v1", torch_dtype=torch.float16, local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained("togethercomputer/RedPajama-INCITE-Instruct-3B-v1")
+model = AutoModelForCausalLM.from_pretrained("togethercomputer/RedPajama-INCITE-Instruct-3B-v1", torch_dtype=torch.float16)
 model = model.to('cuda:0')
 # infer
 prompt = "Q: The capital of France is?\nA:"
